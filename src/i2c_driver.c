0f55b328bd65fc70ff90da9a7efc4f1a4422c9bf
namedummy568-stack <namedummy568@gmail.com>
2025-09-05 20:33:20 +0530

    Implement initial I2C driver functions

--- src/i2c_driver.c
+++ src/i2c_driver.c
@@ -1,3 +1,16 @@
-/*
- * Empty I2C driver file
- */
+#include "i2c_driver.h"
+
+void i2c_init() {
+    // Initialize I2C peripheral
+    // ...
+}
+
+void i2c_write(uint8_t address, uint8_t *data, uint16_t length) {
+    // Write data to I2C device
+    // ...
+}
+
+void i2c_read(uint8_t address, uint8_t *data, uint16_t length) {
+    // Read data from I2C device
+    // ...
+}
